9bee380ea3d2e6fefed3e0838ff9133ec310ac4d
Иван Гевелев <Zigzag69@ukr.net>
2018-09-13 16:47:23 +0300

    Rebuilt app for xib

---
+++ iDyslexic/iDyslexic/Controllers/MainVC/MainVC.swift
@@ -0,0 +1,17 @@
+//
+//  MainVC.swift
+//  iDyslexic
+//
+//  Created by Иван Гевелев on 13.09.2018.
+//  Copyright © 2018 Heveliev Ivan. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
