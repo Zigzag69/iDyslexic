f65d455faa193ac092d26734ffabaf22d9b91d23
Иван Гевелев <Zigzag69@ukr.net>
2018-09-13 16:31:25 +0300

    Initial Commit

---
+++ iDyslexic/iDyslexic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iDyslexic
+//
+//  Created by Иван Гевелев on 13.09.2018.
+//  Copyright © 2018 Heveliev Ivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
