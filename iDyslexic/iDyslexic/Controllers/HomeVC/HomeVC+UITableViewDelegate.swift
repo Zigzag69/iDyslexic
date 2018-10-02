2b88f16166ad9ad19c0011bb8b3ba1a54ef91e7f
Heveliev Ivan <zigzag69@ukr.net>
2018-10-02 14:57:17 +0300

    Changed logic space between cells

--- iDyslexic/iDyslexic/Controllers/HomeVC/HomeVC+UITableViewDelegate.swift
+++ iDyslexic/iDyslexic/Controllers/HomeVC/HomeVC+UITableViewDelegate.swift
@@ -10,24 +10,25 @@ import UIKit
 
 extension HomeVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
         return container.count
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = Bundle.main.loadNibNamed("TextTableViewCell",
+        let cell = Bundle.main.loadNibNamed("ImageTableViewCell",
                                             owner: self,
-                                            options: nil)?.first as! TextTableViewCell
-//        cell.imageTableViewCell.image = #imageLiteral(resourceName: "icTestImage1")
-        cell.labelTableViewCell.text = "Подписка даёт возможность слушать музыку без аудиорекламы на компьютере и в мобильных устройствах, а также сохранять аудиозаписи в приложении BOOM и слушать их без подключения к интернету."
+                                            options: nil)?.first as! ImageTableViewCell
+        cell.imageTableViewCell.image = #imageLiteral(resourceName: "icTestImage1")
+//        cell.labelTableViewCell.text = "I have been questioned by many people to disclose some of the greatest traffic generating techniques that I know of. I have been questioned by many people to disclose some of the greatest traffic generating techniques that I know of."
         return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-            return 180
+        return 320
+//            return 192
         }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -35,29 +36,13 @@ extension HomeVC: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        switch section {
-        case 0:
             let header = Bundle.main.loadNibNamed("SectionHeaderView",
                                                   owner: self,
                                                   options: nil)?.first as! SectionHeaderView
-            header.viewSectionHeader.layer.shadowColor = UIColor.lightGray.cgColor
-            header.viewSectionHeader.layer.shadowRadius = 5
-            header.viewSectionHeader.layer.shadowOpacity = 0.5
-            header.viewSectionHeader.layer.shadowOffset = CGSize(width: 0, height: 2)
         return header
-        default :
-            let headerView = UIView()
-            headerView.backgroundColor = .clear
-            return headerView
-        }
     }
 
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        switch section {
-        case 0:
-            return 68
-        default:
-            return 12
-        }
+        return 56
     }
 }
