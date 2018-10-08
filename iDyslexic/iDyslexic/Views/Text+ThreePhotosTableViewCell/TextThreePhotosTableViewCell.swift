ff368abf10ef4a6811395d8522b215acc4f8dcc2
Heveliev Ivan <zigzag69@ukr.net>
2018-10-08 16:25:08 +0300

    Added new files

---
+++ iDyslexic/iDyslexic/Views/Text+ThreePhotosTableViewCell/TextThreePhotosTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TextThreePhotosTableViewCell.swift
+//  iDyslexic
+//
+//  Created by Admin on 08.10.2018.
+//  Copyright © 2018 Heveliev Ivan. All rights reserved.
+//
+
+import UIKit
+
+class TextThreePhotosTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ iDyslexic/iDyslexic/Views/TwoPhotosTableViewCell/TwoPhotosTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TwoPhotosTableViewCell.swift
+//  iDyslexic
+//
+//  Created by Admin on 08.10.2018.
+//  Copyright © 2018 Heveliev Ivan. All rights reserved.
+//
+
+import UIKit
+
+class TwoPhotosTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
